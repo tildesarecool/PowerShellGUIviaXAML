006e53d6e849e25b387a04add06c148e836901f8
Tildes <tildesare.cool@gmail.com>
2020-05-21 20:35:53 -0700

    following guide by 'adam the automator' https://adamtheautomator.com/build-powershell-gui/

---
+++ PoshGUI-sample/Main.ps1
@@ -0,0 +1,51 @@
+Add-Type -AssemblyName PresentationFramework
+
+Function Get-FixedDisk {
+    [CmdletBinding()]
+    # This param() block indicates the start of parameters declaration
+    param (
+        <# 
+            This parameter accepts the name of the target computer.
+            It is also set to mandatory so that the function does not execute without specifying the value.
+        #>
+        [Parameter(Mandatory)]
+        [string]$Computer
+    )
+    <#
+        WMI query command which gets the list of all logical disks and saves the results to a variable named $DiskInfo
+    #>
+    $DiskInfo = Get-WmiObject Win32_LogicalDisk -ComputerName $Computer -Filter 'DriveType=3'
+    $DiskInfo
+}
+
+# where is the XAML file?
+$xamlFile = ".\MainWindow.xaml"
+
+#create window
+$inputXML = Get-Content $xamlFile -Raw
+$inputXML = $inputXML -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace '^<Win.*', '<Window'
+[XML]$XAML = $inputXML
+
+#Read XAML
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+try {
+    $window = [Windows.Markup.XamlReader]::Load( $reader )
+} catch {
+    Write-Warning $_.Exception
+    throw
+}
+
+# Create variables based on form control names.
+# Variable will be named as 'var_<control name>'
+
+$xaml.SelectNodes("//*[@Name]") | ForEach-Object {
+    #"trying item $($_.Name)"
+    try {
+        Set-Variable -Name "var_$($_.Name)" -Value $window.FindName($_.Name) -ErrorAction Stop
+    } catch {
+        throw
+    }
+}
+Get-Variable var_*
+
+$Null = $window.ShowDialog()
