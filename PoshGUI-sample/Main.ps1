e25eaf202306d65d2b3e5fdd1379263fdc37f8e4
Tildes <tildesare.cool@gmail.com>
2020-05-21 20:54:46 -0700

    successfully made the sample XAML GUI with a button event that runs a PS script. Even works against remote pc.

--- PoshGUI-sample/Main.ps1
+++ PoshGUI-sample/Main.ps1
@@ -15,37 +15,53 @@ Function Get-FixedDisk {
         WMI query command which gets the list of all logical disks and saves the results to a variable named $DiskInfo
     #>
     $DiskInfo = Get-WmiObject Win32_LogicalDisk -ComputerName $Computer -Filter 'DriveType=3'
-    $DiskInfo
+   $DiskInfo
 }
 
-# where is the XAML file?
+#where is the XAML file?
 $xamlFile = ".\MainWindow.xaml"
 
 #create window
 $inputXML = Get-Content $xamlFile -Raw
 $inputXML = $inputXML -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace '^<Win.*', '<Window'
-[XML]$XAML = $inputXML
-
+[xml]$XAML = $inputXML
 #Read XAML
+
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 try {
     $window = [Windows.Markup.XamlReader]::Load( $reader )
-} catch {
+}
+catch {
     Write-Warning $_.Exception
     throw
 }
 
-# Create variables based on form control names.
-# Variable will be named as 'var_<control name>'
+#Create variables based on form control names.
+#Variable will be named as 'var_<control name>'
 
 $xaml.SelectNodes("//*[@Name]") | ForEach-Object {
-    #"trying item $($_.Name)"
+    #"trying item $($_.Name)";
     try {
         Set-Variable -Name "var_$($_.Name)" -Value $window.FindName($_.Name) -ErrorAction Stop
     } catch {
         throw
-    }
+   }
 }
+
 Get-Variable var_*
 
+$var_btnQuery.Add_Click( {
+   #clear the result box
+   $var_txtResults.Text = ""
+       if ($result = Get-FixedDisk -Computer $var_txtComputer.Text) {
+           foreach ($item in $result) {
+               $var_txtResults.Text = $var_txtResults.Text + "DeviceID: $($item.DeviceID)`n"
+               $var_txtResults.Text = $var_txtResults.Text + "VolumeName: $($item.VolumeName)`n"
+               $var_txtResults.Text = $var_txtResults.Text + "FreeSpace: $($item.FreeSpace)`n"
+               $var_txtResults.Text = $var_txtResults.Text + "Size: $($item.Size)`n`n"
+           }
+       }       
+   })
+
+$var_txtComputer.Text = $env:COMPUTERNAME
 $Null = $window.ShowDialog()
