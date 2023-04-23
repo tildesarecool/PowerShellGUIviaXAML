d8e2f323ebbddb8ac70011b603e3428b401bfc2d
tildesarecool <tildesare.cool@gmail.com>
2023-04-22 18:16:03 -0700

    added a label

---
+++ extracticons.ps1
@@ -0,0 +1,21 @@
+﻿Add-Type -AssemblyName System.Drawing
+
+# Load the icons from moricons.dll
+
+
+$iconFile = "C:\Windows\System32\moricons.dll"
+#$iconIndex = 2  # index is 0-based, so 2 is the third icon
+$icon = New-Object System.Drawing.Icon($iconFile)
+$icon.Save("icon588579141.ico")
+
+
+
+###############################
+#$iconFile = "$env:SystemRoot\System32\moricons.dll"
+#$icons = [System.Drawing.Icon]::ExtractAssociatedIcons("$env:SystemRoot\System32\moricons.dll")
+#foreach ($icon in $icons) {
+#    $iconFile = "icon" + $icon.Handle + ".ico"
+#    $fs = New-Object System.IO.FileStream($iconFile, [System.IO.FileMode]::Create)
+#    $icon.Save($fs)
+#    $fs.Close()
+#}
--- using forms for ui.ps1
+++ using forms for ui.ps1
@@ -1,14 +1,32 @@
-﻿Add-Type -AssemblyName System.Windows.Forms
+﻿Add-Type -AssemblyName System.Windows.Forms, System.Drawing
 
+# Load the moreicons.dll assembly
+#Add-Type -AssemblyName moreicons
+# to use an icon from moreicons.dll
+#Add-Type -AssemblyName System.Drawing #, PresentationFramework, PresentationCore, WindowsBase
+
+
+############# setting an icon. just cuz (couldn't get the icon to work)
+
+#$icon = New-Object System.Drawing.Icon([Environment]::SystemDirectory + "\moricons.dll, 3")
+#$form.Icon = [System.Windows.Media.Imaging.BitmapFrame]::Create($icon.ToBitmap())
+
+
+#############  setting an icon. just cuz (couldn't get the icon to work)
+
+
+############# first create the window
 $Form = New-Object System.Windows.Forms.Form
 $Form.Text = "Documentor"
 $Form.ClientSize = New-Object System.Drawing.Size(640, 480)
 
+############# input text box at the top
 $TextBox = New-Object System.Windows.Forms.TextBox
 $TextBox.Location = New-Object System.Drawing.Point(10, 10)
 $TextBox.Size = New-Object System.Drawing.Size(300, 20)
 $Form.Controls.Add($TextBox)
 
+############# query button under the text box
 $QButton = New-Object System.Windows.Forms.Button
 $QButton.Text = "Query"
 $QButton.Location = New-Object System.Drawing.Point(10, 50)
@@ -18,14 +36,27 @@ $QButton.Add_Click({
 })
 $Form.Controls.Add($QButton)
 
+############# close window button at the bottom
+
 $CButton = New-Object System.Windows.Forms.Button
 $CButton.Text = "Close Window"
 $CButton.Location = New-Object System.Drawing.Point(10, 400)
 $CButton.Size = New-Object System.Drawing.Size(100, 30)
 $CButton.Add_Click({
-    # button click event handler code goes here
+    $Form.Close()
 })
 $Form.Controls.Add($CButton)
+
+############# create a label to show output of queries
+$InfoLabel = New-Object System.Windows.Forms.Label
+$InfoLabel.Text = "This is the new label"
+$InfoLabel.Location = New-Object System.Drawing.Point(10, 100)
+$InfoLabel.Size = New-Object System.Drawing.Size(600, 200)
+$InfoLabel.BorderStyle = [System.Windows.Forms.BorderStyle]::FixedSingle
+$InfoLabel.AutoSize = $false
+
+$Form.Controls.Add($InfoLabel)
+
 
 
 $Form.ShowDialog() | Out-Null
