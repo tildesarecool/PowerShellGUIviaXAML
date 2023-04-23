e1467d318d353b8cc25ffd96803f475db097ff1f
tildesarecool <tildesare.cool@gmail.com>
2023-04-22 17:08:45 -0700

    working on UI with forms

--- using forms for ui.ps1
+++ using forms for ui.ps1
@@ -1,21 +1,31 @@
 ﻿Add-Type -AssemblyName System.Windows.Forms
 
 $Form = New-Object System.Windows.Forms.Form
-$Form.Text = "My Form"
-$Form.ClientSize = New-Object System.Drawing.Size(400, 300)
+$Form.Text = "Documentor"
+$Form.ClientSize = New-Object System.Drawing.Size(640, 480)
 
 $TextBox = New-Object System.Windows.Forms.TextBox
 $TextBox.Location = New-Object System.Drawing.Point(10, 10)
-$TextBox.Size = New-Object System.Drawing.Size(100, 20)
+$TextBox.Size = New-Object System.Drawing.Size(300, 20)
 $Form.Controls.Add($TextBox)
 
-$Button = New-Object System.Windows.Forms.Button
-$Button.Text = "Click Me"
-$Button.Location = New-Object System.Drawing.Point(10, 50)
-$Button.Size = New-Object System.Drawing.Size(100, 30)
-$Button.Add_Click({
+$QButton = New-Object System.Windows.Forms.Button
+$QButton.Text = "Query"
+$QButton.Location = New-Object System.Drawing.Point(10, 50)
+$QButton.Size = New-Object System.Drawing.Size(100, 30)
+$QButton.Add_Click({
     # button click event handler code goes here
 })
-$Form.Controls.Add($Button)
+$Form.Controls.Add($QButton)
+
+$CButton = New-Object System.Windows.Forms.Button
+$CButton.Text = "Close Window"
+$CButton.Location = New-Object System.Drawing.Point(10, 400)
+$CButton.Size = New-Object System.Drawing.Size(100, 30)
+$CButton.Add_Click({
+    # button click event handler code goes here
+})
+$Form.Controls.Add($CButton)
+
 
 $Form.ShowDialog() | Out-Null
